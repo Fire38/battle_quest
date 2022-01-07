d4e90ecd9d4961991a3ee00f4ed507a45dc21054
Roman <sablin_1992@mail.ru>
2022-01-08 00:46:07 +0800

    Начало проекта

---
+++ frontend_site/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from "react-dom";
+
+
+export const App = () => {
+  return(
+    <div>Приложуха</div>
+  )
+}
+
+
+
+render(<App/>, document.getElementById('app'));
