72e2619e6b18d46881997ed3ae76f97abc2efa4c
Roman <sablin_1992@mail.ru>
2022-01-12 21:50:43 +0800

    add model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
-class Game(models.Model):
-    title = models.CharField(max_length=150)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to="profile_images")
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
+    members = models.ManyToManyField(User)
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=150)
+    date = models.DateTimeField()
+    teams = models.ManyToManyField(Team, related_name="teams")
+    captain = models.BooleanField(default=False)
+    author = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
+
 
